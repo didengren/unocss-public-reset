08c8712c09345fcf410add4daec26c96679e822a
twonian <nianzu.xu@trinapower.com>
2023-10-11 14:14:55 +0800

    fix: 修改海外菜单样式

--- src/index.ts
+++ src/index.ts
@@ -97,24 +97,53 @@ export function overseasAdminStylesReset(option?: OverseasAdminStylesResetOption
               color: ${primaryColor} !important;
             }
             
-            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical .${projBrand}-menu-item-active:not(.${projBrand}-menu-submenu), .${projBrand}-menu-light .${projBrand}-menu-item-selected, .${projBrand}-menu-light .${projBrand}-menu-submenu-active-border {
+            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical .${projBrand}-menu-item-active:not(.${projBrand}-menu-submenu), .${projBrand}-menu-light .${projBrand}-menu-item-selected {
               color: #fff !important;
+              background-color: transparent !important;
             }
 
-            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical .${projBrand}-menu-item-active:not(.${projBrand}-menu-submenu)::before, .${projBrand}-menu-light .${projBrand}-menu-item-selected::before, .${projBrand}-menu-light .${projBrand}-menu-submenu-active-border::before {
+            [data-theme='light'] .${projBrand}-menu-light .${projBrand}-menu-submenu-active-border {
+              color: ${primaryColor} !important;
+              background-color: #fff !important;
+            }
+
+            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical:not(.${projBrand}-menu-collapse) .${projBrand}-menu-item-active:not(.${projBrand}-menu-submenu)::before, .${projBrand}-menu-light:not(.${projBrand}-menu-collapse) .${projBrand}-menu-item-selected::before {
               position: absolute;
               top: 0;
-              left: 0;
+              left: 10px;
               bottom: 0;
-              right: 0;
+              right: 10px;
               display: block;
-              width: calc(100% - 10px -10px);
+              width: calc(100% - 20px);
               border-radius: 8px;
+              z-index: -1;
+              content: '';
+              background-color: ${primaryColor};
+            }
+
+            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical.${projBrand}-menu-collapse > li.${projBrand}-menu-item-active::before, .${projBrand}-menu-light.${projBrand}-menu-vertical.${projBrand}-menu-collapse .${projBrand}-menu-submenu-active::before {
+              content: none;
+            }
+
+            [data-theme='light'] .ant-popover-inner-content .${projBrand}-menu-light .${projBrand}-menu-item-selected::before {
+              position: absolute;
+              top: 6px;
+              left: 6px;
+              bottom: 6px;
+              right: 6px;
+              display: block;
+              width: calc(100% - 12px);
+              border-radius: 8px;
+              z-index: -1;
               content: '';
               background-color: ${primaryColor};
             }
             
-            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical .${projBrand}-menu-item-active:not(.${projBrand}-menu-submenu)::after, .${projBrand}-menu-light .${projBrand}-menu-item-selected::after, .${projBrand}-menu-light .${projBrand}-menu-submenu-active-border::after {
+            [data-theme='light'] .${projBrand}-menu-light.${projBrand}-menu-vertical .${projBrand}-menu-item-active:not(.${projBrand}-menu-submenu)::after, .${projBrand}-menu-light .${projBrand}-menu-item-selected::after, .${projBrand}-menu-light .${projBrand}-menu-submenu-active-border::after, [data-theme='light'] .${projBrand}-menu-light .${projBrand}-menu-submenu-active-border::before {
+              content: none !important;
+            }
+
+            .fjc-menu-dark.fjc-menu-vertical.fjc-menu-collapse > li.fjc-menu-item-active::before, .fjc-menu-dark.fjc-menu-vertical.fjc-menu-collapse .fjc-menu-submenu-active::before {
               content: none !important;
             }
           `
